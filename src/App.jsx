3e1c3776560f268468ab33266ca853d3bb72075f
Munna Ahmed <munnaahmed2025@gmail.com>
2023-06-29 21:48:24 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
+
+const Greeting = () => {
+  return <h1>Hello to React router</h1>;
+};
+
+const About = () => {
+  return <h1>Welcome to about page</h1>;
+};
+
+const Nav = () => {
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </>
+  );
+};
 
 function App() {
   return (
-    <>
-      <p>landing page</p>
-    </>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
